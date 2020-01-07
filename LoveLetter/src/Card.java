5950743fbb3cda4998d8f7e742944d67f9841d2d
Andrew <a09saeyang@gmail.com>
2020-01-06 23:14:51 -0800

    First Commit

---
+++ LoveLetter/src/Card.java
@@ -0,0 +1,55 @@
+
+public class Card {
+	// instance variables - replace the example below with your own
+    private int cardVal;
+    private int cardQuan;
+    private String cardName; 
+    private String cardDesc; 
+
+    /**
+     * Constructor for objects of class Card
+     */
+    public Card(int v,int q, String n, String d)
+    {
+        // Initialize instance variables
+        cardVal = v;
+        cardQuan = q;
+        cardName = n;
+        cardDesc = d;        
+    }
+    
+    public Card(){
+    }
+
+    public String toString(){
+        return cardName + " has a value of " + cardVal + " with " + cardQuan + " number of card(s) in the deck. ";
+    }
+
+    public int getVal()
+    {
+        return cardVal;
+    }
+
+    public void setVal(int cardVal){this.cardVal=cardVal;}
+
+    public int getQuan()
+    {
+        return cardQuan;
+    }
+
+    public void setQuan(int cardQuan){this.cardQuan=cardQuan;}
+
+    public String getName()
+    {
+        return cardName;
+    }
+
+    public void setString (String cardName){ this.cardName = cardName;}
+
+    public String getDesc()
+    {
+        return cardDesc;
+    }
+
+    public void setDesc(String cardDesc){this.cardDesc = cardDesc;}
+}
---
+++ LoveLetter/src/Game.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Random;
+
+public class Game {
+	public static void main(String[] args) {
+		Card testCard = new Card();
+		
+		System.out.println(testCard.toString());	
+	
+	}
+}
